c138e2adca9f4b4db0a81c21fc07c5268d726e26
Joshua3212 <joshua-koeck@gmx.de>
2022-10-30 18:08:59 +0100

    new Integrations methods

---
+++ src/main/java/com/huddu/IntegrationsClient.java
@@ -0,0 +1,14 @@
+package com.huddu;
+
+
+public class IntegrationsClient {
+
+    public Integrations Integrations;
+
+
+    public IntegrationsClient(String integration_id) {
+        Integrations = new Integrations(integration_id);
+
+    }
+
+}
--- src/main/java/com/huddu/utils.java
+++ src/main/java/com/huddu/utils.java
@@ -66,4 +66,61 @@ public class utils {
         return res.body();
     }
 
+
+    // request without token
+    public static Object _request_without_token(String method, String url, JSONObject body) throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+
+        String base_url = "https://api.huddu.io";
+
+        String final_url = base_url + url;
+
+        HttpRequest request;
+
+
+        if (method == "PUT") {
+            request = HttpRequest.newBuilder(URI.create(
+                    final_url
+            )).header("Content-Type", "application/json").PUT(HttpRequest.BodyPublishers.ofString(body.toString())).build();
+        } else if (method == "DELETE") {
+            request = HttpRequest.newBuilder(URI.create(
+                    final_url
+            )).header("Content-Type", "application/json").DELETE().build();
+        } else {
+            // could be a different method... for not everything else is a POST
+            request = HttpRequest.newBuilder(URI.create(
+                    final_url
+            )).header("Content-Type", "application/json").POST(HttpRequest.BodyPublishers.ofString(body.toString())).build();
+        }
+
+
+        HttpResponse res = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+
+        if (res.statusCode() > 299) {
+            System.out.println(res.body().toString());
+        }
+        return res.body();
+    }
+
+    public static Object _request_without_token(String _method, String url) throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+
+        String base_url = "https://api.huddu.io";
+
+        String final_url = base_url + url;
+
+        HttpRequest request = HttpRequest.newBuilder(URI.create(
+                final_url
+        )).header("Content-Type", "application/json").GET().build();
+
+
+        HttpResponse res = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        if (res.statusCode() > 299) {
+            System.out.println(res.body().toString());
+        }
+        return res.body();
+    }
+
 }
